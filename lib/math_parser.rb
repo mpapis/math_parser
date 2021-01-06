0ee00e982fc86d7679bceb168629c0680cf87ff7
Michal Papis <mpapis@gmail.com>
2021-01-06 17:20:24 +0100

    Provide interface

--- lib/math_parser.rb
+++ lib/math_parser.rb
@@ -1,5 +1,18 @@
+require_relative "math_parser/errors"
+require_relative "math_parser/tokenizer"
+require_relative "math_parser/tree_builder"
+require_relative "math_parser/evaluator"
+
 module MathParser
   def self.calculate(string)
-    4
+    tree = parse(string)
+    # MathParser::Validator.validate!(tree)
+    MathParser::Evaluator.evaluate(tree)
+  end
+
+  def self.parse(string)
+    MathParser::TreeBuilder.build(
+      MathParser::Tokenizer.tokenize(string)
+    )
   end
 end
